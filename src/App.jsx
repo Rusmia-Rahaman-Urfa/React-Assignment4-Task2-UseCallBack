5791dd88c90f6b16359bb4f7494811288a9d8fe4
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2025-10-16 22:37:13 +0600

    chore: fix codes

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function App() {
   return (
     <div style={{ fontFamily: 'Inter, sans-serif', maxWidth: '800px', margin: '0 auto', textAlign: 'center' }}>
       <h1>Task-2: useCallback Practice</h1>
-      
       <hr />
-      
       <Parent />
     </div>
   );
